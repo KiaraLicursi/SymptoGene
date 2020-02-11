9d5f50914bc3076559e374303d86e648471a7423
Kiara Licursi <kiaralicursi@kiaras-air.lan>
2020-02-11 00:30:42 -0500

    fixed Typo

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 
 auth = "QWRtaW46YWRtaW4"
 headers = {"Authorization": "Basic" +auth}
-url = "http://localhost:8080/rest/vocabularies/hpo/suggest?input=" + query
+
 
 
 print("Hello! Search for Symptoms:")
 query = input()
 print("searching...")
 print(query)
+
+url = "http://localhost:8080/rest/vocabularies/hpo/suggest?input=" + query
 
 response = requests.get(url, headers=headers)
 print(response.json())

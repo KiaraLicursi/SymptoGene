15591a8f76f78312fbe81cc3b6ea9616652c2dfe
Kiara Licursi <kiaralicursi@kiaras-air.lan>
2020-02-11 00:37:05 -0500

    Updated to deliver more detailed symptoms to search

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ headers = {"Authorization": "Basic" +auth}
 
 print("Hello! Search for Symptoms:")
 query = input()
-print("searching...")
+print("SEARCHING...")
 print(query)
 
 url = "http://localhost:8080/rest/vocabularies/hpo/suggest?input=" + query
 
 response = requests.get(url, headers=headers)
-print(response.json())
+
+results = response.json()['rows']
+print()
+print('RESULTS:\n')
+for symptom in results :
+	print (symptom['name'])
+
+
 
 
 

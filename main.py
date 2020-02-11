276111ad39edca66d0a6e8b559c87e6c8538eff9
Kiara Licursi <kiaralicursi@kiaras-air.lan>
2020-02-11 00:27:10 -0500

    Allow users to change url based on query

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import requests
 
 auth = "QWRtaW46YWRtaW4"
 headers = {"Authorization": "Basic" +auth}
-url = "http://localhost:8080/rest/vocabularies/hpo/suggest?input=bulb"
+url = "http://localhost:8080/rest/vocabularies/hpo/suggest?input=" + query
+
+
+print("Hello! Search for Symptoms:")
+query = input()
+print("searching...")
+print(query)
 
 response = requests.get(url, headers=headers)
-print(response.status_code)
+print(response.json())
+
+
+
